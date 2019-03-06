c24c5fbac986b33779c80464304d52ff89305828
Amin Abdisamad <theaminux@gmail.com>
2019-03-06 18:04:56 +0300

    added try except block

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import requests
 USER_EMAIL = config.USER_EMAIL
 DEVICE_PASS = config.DEVICE_PASS 
 WEBSITE = config.WEBSITE
+REC_EMAIL = config.RE_EMAIL
 
-r = requests.get("", timeout=5)
-if r.status_code != 200:
+def nofity():
     with smtplib.SMTP('smtp.gmail.com',587) as smtp:
         smtp.ehlo()
         smtp.starttls()
@@ -18,6 +18,13 @@ if r.status_code != 200:
         subject = "SOMETHING WENT WRONG WITH YOUR WEBSITE!!"
         body = "Your Website is down please check, backup and restart the server"
         msg = "Subject: {} \n\n {}".format(subject,body)
-        smtp.sendmail(USER_EMAIL,'theaminux@gmail.com',msg)
+        smtp.sendmail(USER_EMAIL,REC_EMAIL,msg)
+try:
+    r = requests.get(WEBSITE, timeout=5)
+    if r.status_code != 200:
+        nofity()
+except Exception as e:
+     nofity()
 
-
+   
+    
